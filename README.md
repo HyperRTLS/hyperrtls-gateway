265a3ee465f2e90e988a2714bb36fb85324ae37c
Aleksander Wójtowicz <a.wojtowicz@avsystem.com>
2022-10-01 21:24:06 +0200

    feat: gw initial code; seems to not crash

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 ```sh
 west build -b qemu_x86 -p -- -DCONF_FILE=prj_gw.conf
-sudo tools/btproxy -u -i 0
-sudo hciconfig hci0 down
+sudo hciconfig hci1 down
+# Needs to be self-compiled if btproxy isn't shipped with BlueZ by default
+sudo ~/work/bluez/tools/btproxy -u -i 1
 west build -t run
+# TODO: Figure out how to remotely debug it, as west flash isn't working
 ```
--- include/models/gw.h
+++ include/models/gw.h
@@ -3,9 +3,26 @@
 
 #include <models/common.h>
 
+struct hrtls_model_gw_location {
+    float x;
+    float y;
+    float z;
+} __packed;
+
+typedef void hrlts_model_gw_loc_push_handler_t(uint16_t sender_addr, const struct hrtls_model_gw_location *location);
+
+struct hrtls_model_gw_handlers {
+    hrlts_model_gw_loc_push_handler_t *push;
+};
+
+extern const struct bt_mesh_model_op hrtls_model_gw_ops[];
+
 #define HRTLS_MODEL_GW_ID 0x0001
+
 #define HRTLS_MODEL_GW_NODE_REG_OPCODE BT_MESH_MODEL_OP_3(0x01, HRTLS_COMPANY_ID)
+
 #define HRTLS_MODEL_GW_LOC_PUSH_OPCODE BT_MESH_MODEL_OP_3(0x02, HRTLS_COMPANY_ID)
+#define HRTLS_MODEL_GW_LOC_PUSH_LEN sizeof(struct hrtls_model_gw_handlers)
 
 #define HRTLS_MODEL_GW(handlers) \
   BT_MESH_MODEL_VND_CB( \
@@ -15,17 +32,3 @@
     NULL, \
     handlers, \
     NULL)
-
-struct hrtls_model_gw_location {
-    float x;
-    float y;
-    float z;
-};
-
-typedef void hrlts_model_gw_loc_push_handler_t(uint16_t sender_addr, const struct hrtls_model_gw_location *location);
-
-struct hrtls_model_gw_handlers {
-    hrlts_model_gw_loc_push_handler_t *push;
-};
-
-extern const struct bt_mesh_model_op hrtls_model_gw_ops[];
